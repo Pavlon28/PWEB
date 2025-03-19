f9f4307eb64240514dfb0f191dc859bded4ea966
Pavel <pavel.tapu23@gmail.com>
2025-03-19 11:36:09 +0200

    Filter HTTP response to print only the content body

--- src/go2web.py
+++ src/go2web.py
@@ -1,7 +1,6 @@
 import socket
 
 def fetch_url(url):
-    # Extract host and path
     if url.startswith("http://"):
         url = url[7:]
     elif url.startswith("https://"):
@@ -11,7 +10,6 @@ def fetch_url(url):
     host, path = url.split("/", 1) if "/" in url else (url, "")
     path = "/" + path if path else "/"
 
-    # Open a socket and connect to the host
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, 80))
         request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
@@ -24,8 +22,17 @@ def fetch_url(url):
                 break
             response += chunk
 
-    # Print the response (headers + content)
-    print(response.decode(errors="ignore"))
+    # Convert response from bytes to string
+    response_text = response.decode(errors="ignore")
+
+    # Split headers and body
+    if "\r\n\r\n" in response_text:
+        headers, body = response_text.split("\r\n\r\n", 1)
+    else:
+        body = response_text  # If no headers are found, treat everything as the body
+
+    print(body)  # Print only the HTML body
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 2 and sys.argv[1] == "-u":
